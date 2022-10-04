6c5c2082a88fdf2327f95bcdc4e4a42351d58cfa
kakatofoli <kaique.spagnol@gmail.com>
2022-10-04 00:53:10 -0300

    Add files via upload

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6dc236f942851c18102a0ab0d50d14c2.js"
+  "/precache-manifest.69f9f4b707521f20a84057e484d9c7a5.js"
 );
 
 workbox.clientsClaim();
